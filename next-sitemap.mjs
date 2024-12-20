391d5e7b713850f5221371b64910cab7a00551e9
avinavsharma <avinavsharma007@gmail.com>
2024-12-20 18:54:51 +0530

    Added a sitemap.xml to app directory

--- next-sitemap.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-    siteUrl: 'http://localhost:3000',
-    changefreq: 'daily',
-    priority: 0.7,
-    sitemapSize: 7000,
-    exclude: ['/createproduct', '/createorder', '/dashboard', '/orders'],
-    generateRobotsTxt: false,
-}
